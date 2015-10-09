f3f965928465f49db067ef05aaaa87af9083128f
Seth Allen <seth.a@me.com>
2015-10-09 19:04:27 +0000

    Styled student course view page

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,8 @@
 class Lesson < ActiveRecord::Base
 	belongs_to :section
 	mount_uploader :video, VideoUploader
+
+	validates :title, :presence => true
+	validates :subtitle, :presence => true
+	validates :video, :presence => true
 end
