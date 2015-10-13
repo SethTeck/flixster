341ef399f8b8652bf084c32bc4f54225bc41d168
Seth Allen <seth.a@me.com>
2015-10-13 01:32:47 +0000

    Added ranked-model gem to list lessons in proper order

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,7 @@ class Lesson < ActiveRecord::Base
 	validates :title, :presence => true
 	validates :subtitle, :presence => true
 	validates :video, :presence => true
+
+	include RankedModel
+	ranks :row_order, :with_same => :section_id
 end
