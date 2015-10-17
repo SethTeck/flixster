533bfe74d7ca464fcfed9ae1e0a09f216b4478db
Seth Allen <seth.a@me.com>
2015-10-17 01:26:14 +0000

    Added section drag arrangment functionality

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::LessonsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :require_authorized_for_current_section, :only => [:new, :create]
+	before_action :require_authorized_for_current_lesson, :only => [:update]
 
 	def new
 		@lesson = Lesson.new
@@ -23,7 +24,7 @@ class Instructor::LessonsController < ApplicationController
 	private
 
 	def require_authorized_for_current_lesson
-		if current.lesson.section.course.user != current_user
+		if current_lesson.section.course.user != current_user
 			render :text => "Unauthorized", :status => :unauthorized
 		end
 	end
